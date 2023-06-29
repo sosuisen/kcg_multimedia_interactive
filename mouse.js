1b1a05381b6320d139f84ba9983023652b71f3da
sosuisen <hidekazu.kubota@gmail.com>
2023-06-29 19:22:23 +0900

    fix: use parent and child

--- mouse.js
+++ mouse.js
@@ -12,25 +12,26 @@ gsap.ticker.add(time => {
 
 document.body.appendChild(app.view);
 
-// largeObjがマウスに反応するようにする
-const largeObj = new PIXI.Graphics();
-largeObj.beginFill(0xffb090);
-largeObj.drawRoundedRect(0, 0, 480, 360);
-largeObj.endFill();
-largeObj.position.set(80, 60);
-largeObj.eventMode = 'dynamic';
-largeObj.on('pointermove', (event) => {
-    console.log(`[stage] screen(${event.screen.x}, ${event.screen.y}))`);
-    // greenStarはapp.stageの子なので、スクリーン座標を指定する。
+// parentObjがマウスに反応するようにする
+const parentObj = new PIXI.Graphics();
+parentObj.beginFill(0xffb090);
+parentObj.drawRoundedRect(0, 0, 480, 360);
+parentObj.endFill();
+parentObj.position.set(80, 60);
+parentObj.eventMode = 'dynamic';
+parentObj.on('pointermove', event => {
+    const localPos = event.data.getLocalPosition(event.currentTarget);
+    console.log(`[parent] screen(${event.screen.x}, ${event.screen.y}), local(${localPos.x}, ${localPos.y}))`);
+    // greenStarはapp.stageの子なので、グローバル座標を指定する。
     // マウスの位置へ移動
     // greenStar.position = event.screen;
     // マウスの位置までアニメーションさせるとよりスムーズ
     // gsap.to(greenStar, { x: event.screen.x, y: event.screen.y });
 });
-app.stage.addChild(largeObj);
+app.stage.addChild(parentObj);
 
 // app.stageに星を追加。
-// この星はlargeObjより手前、smallObjより奥側になる。
+// この星はparentObjより手前、childObjより奥側になる。
 /*
 const greenStar = PIXI.Sprite.from('greenstar.png');
 greenStar.pivot.set(16, 16);
@@ -41,31 +42,31 @@ gsap.to(greenStar, { duration: 5, repeat: -1, ease: 'none', pixi: {
 }});
 */
 
-// smallObjがマウスに反応するようにする
-const smallObj = new PIXI.Graphics();
-smallObj.beginFill(0xa0a0f0);
-smallObj.drawCircle(0, 0, 120);
-smallObj.position.set(320, 240); 
-smallObj.endFill();
-smallObj.eventMode = 'dynamic';
-smallObj.interactiveChildren = false;
-smallObj.on('pointermove', (event) => {
+// childObjがマウスに反応するようにする
+const childObj = new PIXI.Graphics();
+childObj.beginFill(0xa0a0f0);
+childObj.drawCircle(0, 0, 120);
+childObj.position.set(320, 240); 
+childObj.endFill();
+childObj.eventMode = 'dynamic';
+childObj.interactiveChildren = false;
+childObj.on('pointermove', event => {
     // オブジェクト内のローカル座標系
     // 円の場合、中心が(0, 0)となる。
     const localPos = event.data.getLocalPosition(event.currentTarget);
     console.log(`[child] screen(${event.screen.x}, ${event.screen.y}), local(${localPos.x}, ${localPos.y}))`);
-    // yellowStarはsmallObjの子なので、smallObjのローカル座標を指定する。
+    // yellowStarはchildObjの子なので、childObjのローカル座標を指定する。
     // yellowStar.position = localPos;
     // マウスの位置までアニメーションさせるとよりスムーズ
     // gsap.to(yellowStar, { x: localPos.x, y: localPos.y });
 });
-app.stage.addChild(smallObj);
+app.stage.addChild(childObj);
 
-// smallObjに星を追加
+// childObjに星を追加
 /*
 const yellowStar = PIXI.Sprite.from('yellowstar.png');
 yellowStar.pivot.set(16, 16);
-smallObj.addChild(yellowStar);
+childObj.addChild(yellowStar);
 gsap.to(yellowStar, { duration: 5, repeat: -1, ease: 'none', pixi: {
     rotation: -360,
 }});
