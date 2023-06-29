94aa1f5f1f3b00cebfe5a95a0765669751ee864a
sosuisen <hidekazu.kubota@gmail.com>
2023-06-30 01:03:57 +0900

    fix: atlas

--- kihon.js
+++ kihon.js
@@ -6,117 +6,71 @@ document.body.appendChild(app.view);
 const atlasData = {
 	frames: {
     walkLeft0: {
-			frame: { x: 0, y:252, w:126, h:126 },
-			sourceSize: { w: 126, h: 126 },
-			spriteSourceSize: { x: 0, y: 0, w: 126, h: 126 }
+			frame: { x: 0, y:256, w:128, h:128 },
 		},
     walkLeft1: {
-			frame: { x: 126, y:252, w:126, h:126 },
-			sourceSize: { w: 126, h: 126 },
-			spriteSourceSize: { x: 0, y: 0, w: 126, h: 126 }
+			frame: { x: 128, y:256, w:128, h:128 },
 		},
     walkLeft2: {
-			frame: { x: 252, y:252, w:126, h:126 },
-			sourceSize: { w: 126, h: 126 },
-			spriteSourceSize: { x: 0, y: 0, w: 126, h: 126 }
+			frame: { x: 256, y:256, w:128, h:128 },
 		},
     walkRight0: {
-			frame: { x: 378, y:252, w:126, h:126 },
-			sourceSize: { w: 126, h: 126 },
-			spriteSourceSize: { x: 0, y: 0, w: 126, h: 126 }
+			frame: { x: 384, y:256, w:128, h:128 },
 		},
     walkRight1: {
-			frame: { x: 504, y:252, w:126, h:126 },
-			sourceSize: { w: 126, h: 126 },
-			spriteSourceSize: { x: 0, y: 0, w: 126, h: 126 }
+			frame: { x: 512, y:256, w:128, h:128 },
 		},
     walkRight2: {
-			frame: { x: 630, y:252, w:126, h:126 },
-			sourceSize: { w: 126, h: 126 },
-			spriteSourceSize: { x: 0, y: 0, w: 126, h: 126 }
+			frame: { x: 640, y:256, w:128, h:128 },
 		},    
     walkDown0: {
-			frame: { x: 0, y:126, w:126, h:126 },
-			sourceSize: { w: 126, h: 126 },
-			spriteSourceSize: { x: 0, y: 0, w: 126, h: 126 }
+			frame: { x: 0, y:128, w:128, h:128 },
 		},
     walkDown1: {
-			frame: { x: 126, y:126, w:126, h:126 },
-			sourceSize: { w: 126, h: 126 },
-			spriteSourceSize: { x: 0, y: 0, w: 126, h: 126 }
+			frame: { x: 128, y:128, w:128, h:128 },
 		},
     walkDown2: {
-			frame: { x: 252, y:126, w:126, h:126 },
-			sourceSize: { w: 126, h: 126 },
-			spriteSourceSize: { x: 0, y: 0, w: 126, h: 126 }
+			frame: { x: 256, y:128, w:128, h:128 },
 		},
     walkDown0: {
-			frame: { x: 0, y:126, w:126, h:126 },
-			sourceSize: { w: 126, h: 126 },
-			spriteSourceSize: { x: 0, y: 0, w: 126, h: 126 }
+			frame: { x: 0, y:128, w:128, h:128 },
 		},
     walkDown1: {
-			frame: { x: 126, y:126, w:126, h:126 },
-			sourceSize: { w: 126, h: 126 },
-			spriteSourceSize: { x: 0, y: 0, w: 126, h: 126 }
+			frame: { x: 128, y:128, w:128, h:128 },
 		},
     walkDown2: {
-			frame: { x: 252, y:126, w:126, h:126 },
-			sourceSize: { w: 126, h: 126 },
-			spriteSourceSize: { x: 0, y: 0, w: 126, h: 126 }
+			frame: { x: 256, y:128, w:128, h:128 },
 		},
     walkUp0: {
-			frame: { x: 378, y:126, w:126, h:126 },
-			sourceSize: { w: 126, h: 126 },
-			spriteSourceSize: { x: 0, y: 0, w: 126, h: 126 }
+			frame: { x: 384, y:128, w:128, h:128 },
 		},
     walkUp1: {
-			frame: { x: 504, y:126, w:126, h:126 },
-			sourceSize: { w: 126, h: 126 },
-			spriteSourceSize: { x: 0, y: 0, w: 126, h: 126 }
+			frame: { x: 512, y:128, w:128, h:128 },
 		},
     walkUp2: {
-			frame: { x: 630, y:126, w:126, h:126 },
-			sourceSize: { w: 126, h: 126 },
-			spriteSourceSize: { x: 0, y: 0, w: 126, h: 126 }
+			frame: { x: 640, y:128, w:128, h:128 },
 		},    
     fly0: {
-			frame: { x: 0, y:0, w:126, h:126 },
-			sourceSize: { w: 126, h: 126 },
-			spriteSourceSize: { x: 0, y: 0, w: 126, h: 126 }
+			frame: { x: 0, y:0, w:128, h:128 },
 		},
     fly1: {
-			frame: { x: 126, y:0, w:126, h:126 },
-			sourceSize: { w: 126, h: 126 },
-			spriteSourceSize: { x: 0, y: 0, w: 126, h: 126 }
+			frame: { x: 128, y:0, w:128, h:128 },
 		},
     fly2: {
-			frame: { x: 252, y:0, w:126, h:126 },
-			sourceSize: { w: 126, h: 126 },
-			spriteSourceSize: { x: 0, y: 0, w: 126, h: 126 }
+			frame: { x: 256, y:0, w:128, h:128 },
 		},
     fly3: {
-			frame: { x: 378, y:0, w:126, h:126 },
-			sourceSize: { w: 126, h: 126 },
-			spriteSourceSize: { x: 0, y: 0, w: 126, h: 126 }
+			frame: { x: 384, y:0, w:128, h:128 },
 		},
     fly4: {
-			frame: { x: 504, y:0, w:126, h:126 },
-			sourceSize: { w: 126, h: 126 },
-			spriteSourceSize: { x: 0, y: 0, w: 126, h: 126 }
+			frame: { x: 512, y:0, w:128, h:128 },
 		},
     fly5: {
-			frame: { x: 630, y:0, w:126, h:126 },
-			sourceSize: { w: 126, h: 126 },
-			spriteSourceSize: { x: 0, y: 0, w: 126, h: 126 }
+			frame: { x: 640, y:0, w:128, h:128 },
 		},
-
 	},
 	meta: {
 		image: 'hiyoco.png',
-		format: 'RGBA8888',
-		size: { w: 768, h: 384 },
-		scale: 1
 	},
 	animations: {
 		walkLeft: ['walkLeft0','walkLeft1','walkLeft2'],
@@ -145,27 +99,27 @@ app.stage.addChild(walkLeft);
 const walkRight = new PIXI.AnimatedSprite(spritesheet.animations.walkRight);
 walkRight.animationSpeed = 0.1;
 walkRight.play();
-walkRight.x = 126;
+walkRight.x = 128;
 app.stage.addChild(walkRight);
 
 const walkDown = new PIXI.AnimatedSprite(spritesheet.animations.walkDown);
 walkDown.animationSpeed = 0.1;
 walkDown.play();
 walkDown.x = 0;
-walkDown.y = 126;
+walkDown.y = 128;
 app.stage.addChild(walkDown);
 
 const walkUp = new PIXI.AnimatedSprite(spritesheet.animations.walkUp);
 walkUp.animationSpeed = 0.1;
 walkUp.play();
-walkUp.x = 126;
-walkUp.y = 126;
+walkUp.x = 128;
+walkUp.y = 128;
 app.stage.addChild(walkUp);
 
 const fly = new PIXI.AnimatedSprite(spritesheet.animations.fly);
 fly.animationSpeed = 0.1;
 fly.play();
 fly.x = 0;
-fly.y = 252;
+fly.y = 256;
 app.stage.addChild(fly);
 
